a792047be83379d0613600fc35847a99d05f3fbe
franco.stramana <franco.stramana@gmail.com>
2021-05-30 13:22:40 -0300

    Add mocha js to testing

---
+++ src/test/cli.spec.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const init = require('../libs/init');
+
+describe('cli', function() {
+
+  describe('#hello', function() {
+    it('should return true', async function() {
+        assert.equal(true, true);
+    });
+  });
+
+});
